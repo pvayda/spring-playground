02d52482155de01d2a6848c429f75efd3bda0dfc
Peter Vayda <petertvayda@gmail.com>
2020-12-09 12:55:44 -0800

    added getFlights endpoint

--- src/main/java/com/galvanize/springplayground/FlightController.java
+++ src/main/java/com/galvanize/springplayground/FlightController.java
@@ -4,14 +4,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 import static java.util.Calendar.APRIL;
 
 @RestController
 public class FlightController {
     @GetMapping("/flights/flight")
-    public Flight getFlight(){ {
+    public Flight getFlight(){
         Flight flight = new Flight();
         Date newDate = new Date(2017-1900, APRIL, 21, 14,34);
         flight.setDeparts(newDate);
@@ -20,7 +22,26 @@ public class FlightController {
         Flight.Ticket ticket = new Flight.Ticket(Arrays.asList(person1,person2), 200);
         flight.setTickets(Arrays.asList(ticket));
         return flight;
-        }
+
+    }
+    @GetMapping("/flights")
+    public List<Flight> getFlights(){
+        Flight flight1 = new Flight();
+        Date newDate = new Date(2017-1900, APRIL, 21, 14,34);
+        flight1.setDeparts(newDate);
+        Flight.Person person1 = new Flight.Person("Jon", "Snow");
+        Flight.Person person2 = new Flight.Person("Rob", "Stark");
+        Flight.Ticket ticket = new Flight.Ticket(Arrays.asList(person1,person2), 200);
+        flight1.setTickets(Arrays.asList(ticket));
+
+        Flight flight2 = new Flight();
+        Date newDate2 = new Date(2017-1900, Calendar.FEBRUARY, 21, 14,34);
+        flight2.setDeparts(newDate2);
+        Flight.Person person3 = new Flight.Person("Luke", "Skywalker");
+        Flight.Person person4 = new Flight.Person("Han", "Solo");
+        Flight.Ticket ticket2 = new Flight.Ticket(Arrays.asList(person3,person4), 400);
+        flight2.setTickets(Arrays.asList(ticket2));
+        return Arrays.asList(flight1, flight2);
     }
 
 }
--- src/test/java/com/galvanize/springplayground/FlightControllerTest.java
+++ src/test/java/com/galvanize/springplayground/FlightControllerTest.java
@@ -42,5 +42,24 @@ public class FlightControllerTest {
                 .andExpect(jsonPath("$.tickets.[0].price", is(200)));
 
     }
+     //'/flights'
+     @Test
+     public void testGetFlights() throws Exception{
+         RequestBuilder request = MockMvcRequestBuilders.get("/flights")
+                 .accept(MediaType.APPLICATION_JSON_UTF8)
+                 .contentType(MediaType.APPLICATION_JSON_UTF8);
+         this.mvc.perform(request)
+                 .andExpect(status().isOk())
+                 .andExpect(jsonPath("$[0].departs", is("2017-04-21 14:34")))
+                 .andExpect(jsonPath("$[0].tickets.[0].passengers.[0].firstName", is("Jon")))
+                 .andExpect(jsonPath("$[0].tickets.[0].passengers.[0].lastName", is("Snow")))
+                 .andExpect(jsonPath("$[0].tickets.[0].price", is(200)))
+                 .andExpect(jsonPath("$[1].departs", is("2017-02-21 14:34")))
+                 .andExpect(jsonPath("$[1].tickets.[0].passengers.[0].firstName", is("Luke")))
+                 .andExpect(jsonPath("$[1].tickets.[0].passengers.[0].lastName", is("Skywalker")))
+                 .andExpect(jsonPath("$[1].tickets.[0].price", is(400)));
+
+
+     }
 
 }
