d7413af295ccf09a2a303fde9b9b7858d4b72de8
Peter Vayda <petertvayda@gmail.com>
2020-12-11 12:06:02 -0800

    added lessons to playground

---
+++ src/main/java/com/galvanize/springplayground/LessonRepository.java
@@ -0,0 +1,8 @@
+package com.galvanize.springplayground;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface LessonRepository extends CrudRepository<Lesson, Long> {
+
+
+}
